8db9f468786dabf48c12e0d7ec31e7f9e22346ff
Alex Fisher <asfisher961@gmail.com>
2017-01-24 03:30:39 -0500

    Added first version of TweeterServlet Moved static resources Updated index.html

--- src/com/fisher/pitttweeter/TweeterServlet.java
+++ src/com/fisher/pitttweeter/TweeterServlet.java
@@ -2,11 +2,39 @@ package com.fisher.pitttweeter;
 
 import java.io.IOException;
 import javax.servlet.http.*;
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
 
 @SuppressWarnings("serial")
 public class TweeterServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		try 
+        {
+            // configuring twitter4j
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            cb.setOAuthConsumerKey("x");
+            cb.setOAuthConsumerSecret("x");
+            cb.setOAuthAccessToken("x");
+            cb.setOAuthAccessTokenSecret("x");
+            // get Twitter instance
+            Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+            // get tweet
+            String newTweet = request.getParameter("text");
+            if ((newTweet.length() > 140) || (newTweet.isEmpty()))
+            {
+            	// invalid tweet, respond with error
+            	// user should hopefully never see this
+            }
+            // tweeting
+            twitter.updateStatus(newTweet);
+            // send success response
+        }
+        catch(TwitterException te)
+        {
+            te.printStackTrace();
+            System.out.println("Twitter4j Error: " + te.getMessage());
+            System.exit(-1);
+        }
 	}
 }
